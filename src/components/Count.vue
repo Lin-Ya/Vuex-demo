d6d68efd0e2adffec5876136af25bb047acf1e68
Lin-Ya <fenghuiwing@outlook.com>
2018-03-29 19:53:08 +0800

    vuex引入，建立store数据仓库用于管理数据和状态

---
+++ src/components/Count.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <div>
+        <h2>{{msg}}</h2>
+        <hr/>
+        <h3>{{$store.state.count}}</h3>
+    </div>
+</template>
+<script>
+    import store from '@/vuex/store'        //引用数据仓库
+    export default{
+        data(){
+            return{
+                msg:'Hello Vuex',
+            }
+        },
+        store
+    }
+</script>
---
+++ src/vuex/store.js
@@ -0,0 +1,16 @@
+//这个store是数据商店的意思。，和状态管理器
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const state={
+    count:3
+}
+
+
+export default new Vuex.Store({
+	state   //状态
+ 
+})
