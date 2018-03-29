5f6c8352405e4431cd8a572d15d33211dddd1229
Lin-Ya <fenghuiwing@outlook.com>
2018-03-29 20:05:14 +0800

    利用commit调用store中设置的mutations的方法，改变state

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -5,6 +5,8 @@
         <hr/>
         <h3>{{$store.state.count}}</h3>
         <div>
+            <!-- 需要改变状态，需要使用到cmooit发出通知调用mutations方法。 -->
+            <!-- 注意，这里操作的是公用的数据仓库store.js里面的数据 -->
             <button v-on:click="$store.commit('add')">添加</button>
             <button v-on:click="$store.commit('reduce')">减少</button>
         </div>
