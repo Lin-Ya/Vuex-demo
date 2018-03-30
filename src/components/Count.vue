397d6217d89e7b41cddadc52b6c9f403c78aa4e9
Lin-Ya <fenghuiwing@outlook.com>
2018-03-30 16:17:19 +0800

    两种使用mutations的方法。一个是使用commit，另一种则是把其变成当前常亮。

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -8,7 +8,7 @@
         <div>
             <!-- 需要改变状态，需要使用到cmooit发出通知调用mutations方法。 -->
             <!-- 注意，这里操作的是公用的数据仓库store.js里面的数据 -->
-            <button v-on:click="add(10)">添加</button>
+            <button v-on:click="$store.commit('add',10)">添加</button>
             <button v-on:click="reduce">减少</button>
         </div>
     </div>
