803bdd78687b15b5723b136f3a42a4ddc06342b6
Lin-Ya <fenghuiwing@outlook.com>
2018-03-30 16:14:50 +0800

    利用mapMutations传递store.js中的mutations。写在methods中

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -8,20 +8,21 @@
         <div>
             <!-- 需要改变状态，需要使用到cmooit发出通知调用mutations方法。 -->
             <!-- 注意，这里操作的是公用的数据仓库store.js里面的数据 -->
-            <button v-on:click="$store.commit('add',10)">添加</button>
-            <button v-on:click="$store.commit('reduce')">减少</button>
+            <button v-on:click="add(10)">添加</button>
+            <button v-on:click="reduce">减少</button>
         </div>
     </div>
 </template>
 <script>
     import store from '@/vuex/store'        //引用数据仓库
-    import {mapState} from 'vuex';          //需要加中括号
+    import {mapState, mutations, mapMutations} from 'vuex';          //需要加中括号
     export default{
         data(){
             return{
                 msg:'Hello Vuex',
             }
         },
+        methods:mapMutations(['add','reduce']),
         //当映射的计算属性的名称与 state 的子节点名称相同时，我们也可以给 mapState 传一个字符串数组。
         computed: mapState(["count"]), 
         store
